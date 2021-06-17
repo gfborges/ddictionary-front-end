bfeea5c8cbb2b04a8cc8405102f06a2f2b8e5fc1
Gabriel Borges <gabrielfr.borges@gmail.com>
2021-06-16 23:20:19 -0300

    delete preloaded things

---
+++ pages/_domain/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="12" md="8">
+      <v-card class="" width="100%">
+        <v-card-title class="headline">
+          {{ $route.params.domain }}
+        </v-card-title>
+        <v-card-text></v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  components: {},
+}
+</script>
